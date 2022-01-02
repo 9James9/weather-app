4640934db34cbab3beb30dc993f2133801899733
James Driscoll <thejamesdriscoll@gmail.com>
2022-01-01 19:15:03 -0600

    added button to switch between celsius and fahrenheit

--- script.js
+++ script.js
@@ -2,30 +2,65 @@ const cityInput = document.querySelector('[data-city-input]')
 const searchButton = document.querySelector('[data-search-button]')
 const cityDisplay = document.querySelector('[data-city-display]')
 const selectedCity = document.querySelector('[data-selected-city]')
+const convertBtn = document.querySelector('[data-convert-celsius]')
 searchButton.addEventListener('click', e => {
     e.preventDefault()
     let city = cityInput.value
-    getWeather(city)
+    getWeather(city,'fahrenheit')
+    // cityInput.value = ''
 })
+convertBtn.addEventListener('click', () => {
+    let city = cityInput.value
+    if (convertBtn.textContent == 'Convert to Celsius') {
+        getWeather(city,'celsius')
+        convertBtn.textContent = 'Convert to Fahrenheit'
+    } else if (convertBtn.textContent == 'Convert to Fahrenheit') {
+        getWeather(city,'fahrenheit')
+        convertBtn.textContent = 'Convert to Celsius'
+    }
 
-async function getWeather(city) {
+})
+async function getWeather(city,unit) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=eb40cd8fe7b3bb3460776fbee82754b2`, {
         mode: 'cors'
     })
     response.json().then(response => {
         console.log(response)
-        cityDisplay.textContent = `The current weather is ${kelvinToFahrenheit(response.main.temp)} degrees fahrenheit with a high of ${kelvinToFahrenheit(response.main.temp_max)} and a low of ${kelvinToFahrenheit(response.main.temp_min)}. Expect wind speeds around ${response.wind.speed} miles per hour`
-        selectedCity.textContent = `${toCaps(city)}`
+        if (unit == 'fahrenheit') {
+            displayCityWeather('Fahrenheit',
+                kelvinToFahrenheit(response.main.temp),
+                kelvinToFahrenheit(response.main.temp_max),
+                kelvinToFahrenheit(response.main.temp_min),
+                convertMsToMph(response.wind.speed))
+        } else if (unit == 'celsius') {
+            displayCityWeather('Celsius',
+                kelvinToCelsius(response.main.temp),
+                kelvinToCelsius(response.main.temp_max),
+                kelvinToCelsius(response.main.temp_min),
+                convertMsToMph(response.wind.speed)
+            )
+        }
+
+        displayCurrentCity(response.name)
     })
+}
+function displayCityWeather(unit,currentTemp,highTemp,lowTemp,windSpeeds) {
+    cityDisplay.textContent = `The current weather is ${currentTemp} degrees ${unit} with a high of ${highTemp} and a low of ${lowTemp}. Expect wind speeds around ${windSpeeds} miles per hour`
+}
+function displayCurrentCity(city) {
+    selectedCity.textContent = `${toCaps(city)}`
 }
 function toCaps(string) {
     return string.replace(/\b\w/g, letter => {
         return letter.toUpperCase()
     })
 }
-// function kelvinToCelsius(kelvin) {
-//     return kelvin - 273.15
-// }
+function convertMsToMph(ms) {
+    return (ms * 2.236936).toFixed(1)
+}
+function kelvinToCelsius(kelvin) {
+    return (kelvin - 273.15).toFixed(1)
+}
 
 // function celsiusToFahrenheit(celsius) {
 //     return (celsius * 9 / 5) + 32
@@ -34,4 +69,4 @@ function toCaps(string) {
 function kelvinToFahrenheit(kelvin) {
     return (((kelvin - 273.15) * 9 / 5) + 32).toFixed(0)
 }
-getWeather('las vegas')
+// getWeather('las vegas')
